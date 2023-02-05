97ea7a19b5c401b88840bc1ae08f41b9f068544f
omriv88 <113102456+omriv88@users.noreply.github.com>
2023-02-05 12:55:59 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 my_ip=$(curl http://checkip.amazonaws.com)
-
 curl --insecure http://$my_ip:8080/jnlpJars/jenkins-cli.jar --output jenkins-cli.jar
 chmod +x jenkins-cli.jar
 java -jar jenkins-cli.jar -s http://$my_ip:8080 -webSocket create-job "pull request demo" < freestyle-job.xml
