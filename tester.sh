bfc8d83b7b16e6186cdd42edd2c5d6c6b841035a
omriv88 <113102456+omriv88@users.noreply.github.com>
2023-02-09 15:58:00 +0200

    Create tester.sh

---
+++ tester.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+git clone https://github.com/omriv88/deploy-jenkins-jobs.git  
+cp -r -f deploy-jenkins-jobs1/* ./
+
+
+while true; do
+  # URL to check
+#my_ip=$(curl http://checkip.amazonaws.com)
+  url="54.160.65.219:8080"
+
+  # Check if URL is working
+  response_code=$(curl -s -o /dev/null -w "%{http_code}" $url)
+  if [ $response_code -eq 200 ]; then
+    # URL is working, run the test script
+    bash ./test.sh
+  else
+    # URL is not working
+    echo "Error: URL is not working. Response code: $response_code"
+  fi
+
+  # Wait for 2 seconds before checking again
+  sleep 2
+done
