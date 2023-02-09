bd737bd34610f3c009c8f0c2f5df491f83e9e7ee
omriv88 <113102456+omriv88@users.noreply.github.com>
2023-02-09 14:08:58 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sleep 25s
 git clone https://github.com/omriv88/deploy-jenkins-jobs.git
 cp -r -f /deploy-jenkins-jobs/* ./
 my_ip=$(curl http://checkip.amazonaws.com)
+sleep 25s
 curl --insecure http://$my_ip:8080/jnlpJars/jenkins-cli.jar --output jenkins-cli.jar
 chmod +x jenkins-cli.jar
 java -jar jenkins-cli.jar -s http://$my_ip:8080 -webSocket create-job "pull request demo" < freestyle-job.xml
