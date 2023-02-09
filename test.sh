d75a1c078d3848d076ea4c9f73e48930749f6363
omriv88 <113102456+omriv88@users.noreply.github.com>
2023-02-09 13:58:48 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sleep 25s
+git clone https://github.com/omriv88/deploy-jenkins-jobs.git
+cp -r -f /deploy-jenkins-jobs/* ./
 my_ip=$(curl http://checkip.amazonaws.com)
 curl --insecure http://$my_ip:8080/jnlpJars/jenkins-cli.jar --output jenkins-cli.jar
 chmod +x jenkins-cli.jar
